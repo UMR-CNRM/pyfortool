592a055e9bea634960f208e7aa8e95364e9e7e0b
Sébastien Riette <sebastien.riette@meteo.fr>
2026-04-09 09:11:44 +0200

    Linting

--- src/pyfortool/applications.py
+++ src/pyfortool/applications.py
@@ -1494,7 +1494,8 @@ class Applications():
                                             # Shumans
                                             var, inested = None, 0
                                             # pylint: disable-next=unsubscriptable-object
-                                            while not var or var['as'] is None or len(var['as']) == 0:
+                                            while (not var or var['as'] is None or
+                                                   len(var['as']) == 0):
                                                 # While the var is not an array already declared
                                                 # callVar[0] is the first array on which the
                                                 # function is applied
